1b89b0eadd9119b1be9a27d305b4c74c6aad5f82
Maria <melnikovamaria1708@gmail.com>
2023-02-01 07:17:58 +0500

    Branch flexible made

--- src/main/java/Radio.java
+++ src/main/java/Radio.java
@@ -1,37 +1,46 @@
 public class Radio {
-    private int radiostation;
+    private int stationsCount;
+    private int lastRadiostation = stationsCount - 1;
+    private int radiostation = lastRadiostation;
+    private int currentVolume;
+
+    public Radio(int stationsCount) {
+        this.stationsCount = stationsCount;
+        this.lastRadiostation = stationsCount - 1;
+    }
+
+    public Radio() {
+        this.stationsCount = 10;
+        this.lastRadiostation = stationsCount - 1;
+    }
 
     public int setRadiostation(int newRadiostation) {
-        if (newRadiostation < 1) {
-            return radiostation = 1;
+        if (newRadiostation <= 0) {
+            return radiostation = 0;
         }
-        if (newRadiostation > 9) {
-            return radiostation = 9;
+        if (newRadiostation > lastRadiostation) {
+            return radiostation = lastRadiostation;
         }
         return radiostation = newRadiostation;
     }
 
     public int nextRadiostation() {
-        if (radiostation >= 9) {
-            return radiostation = 1;
-        } else {
-            return radiostation = radiostation + 1;
+        if (radiostation >= lastRadiostation) {
+            return radiostation = 0;
         }
+        return radiostation = radiostation + 1;
     }
 
     public int prevRadiostation() {
-        if (radiostation <= 1) {
-            return radiostation = 9;
-        } else {
-            return radiostation = radiostation - 1;
+        if (radiostation <= 0) {
+            return radiostation = lastRadiostation;
         }
+        return radiostation = radiostation - 1;
     }
 
-    private int currentVolume;
-
     public int setCurrentVolume(int newCurrentVolume) {
-        if (newCurrentVolume >= 10) {
-            return currentVolume = 10;
+        if (newCurrentVolume >= 100) {
+            return currentVolume = 100;
         }
         if (newCurrentVolume <= 0) {
             return currentVolume = 0;
@@ -40,18 +49,16 @@ public class Radio {
     }
 
     public int increaseVolume() {
-        if (currentVolume < 10) {
+        if (currentVolume < 100) {
             return currentVolume = currentVolume + 1;
-        } else {
-            return currentVolume = 10;
         }
+        return currentVolume = 100;
     }
 
     public int decreaseVolume() {
         if (currentVolume > 0) {
             return currentVolume = currentVolume - 1;
-        } else {
-            return currentVolume = 0;
         }
+        return currentVolume = 0;
     }
 }
